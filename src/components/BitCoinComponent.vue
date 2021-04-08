4e98723f3a6121b742364cf0a74043b7d83f2c1e
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 16:49:41 +0900

    ボタンクリックで表示できるよう変更

--- src/components/BitCoinComponent.vue
+++ src/components/BitCoinComponent.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <h2>本日のビットコイン価格表示</h2>
-    <ul v-cloak>
-      <li v-for="(rate, currency) in bpi" :key="currency">
-        <!-- |の後にfilterを設置 -->
-        {{ currency }} {{ rate.rate_float | currencyDecimal }}
-      </li>
-    </ul>
+      <div v-if="bitCoinFlg">
+      <section v-if="hasError">
+        Error
+      </section>
+      <section v-else>
+        <ul v-cloak>
+        <li v-for="(rate, currency) in bpi" :key="currency">
+          <!-- |の後にfilterを設置 -->
+          {{ currency }} {{ rate.rate_float | currencyDecimal }}
+        </li>
+      </ul>
+      </section>
+      </div>
+      <button @click="change" v-if="!bitCoinFlg">表示</button>
   </div>
 </template>
 
@@ -18,29 +26,35 @@ export default({
   data(){
     return{
       bpi: null,
+      hasError: false,
+      bitCoinFlg: false,
+    }
+  },
+  methods: {
+    change(){
+      console.log(this.bitCoinFlg);
+      this.bitCoinFlg = true
     }
   },
   mounted: function(){
     axios.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     .then(function(response){
-      console.log(response.data)
       this.bpi = response.data.bpi
       // vueインスタンスのthisを指定するためにbindする必要がある
     }.bind(this))
     // エラーのキャッチ
     .catch(function(error){
       console.log(error)
-    })
+      this.hasError = true
+    }.bind(this))
   },
   filters:{
+    // 小数点以下の表示を指定
     currencyDecimal(value){
       // toFixed 小数点以下のけた数を指定
       return value.toFixed(2)
     }
   },
-  methodes: {
-
-  }
 });
 
 </script>
