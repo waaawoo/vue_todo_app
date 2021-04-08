96e369ba41ca99242975eeec515e9ab74f13d6f8
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 12:19:06 +0900

    form入力の双方向バインディング

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,14 +2,24 @@
   <div>
     <h2>TODO List</h2>
     <form v-on:submit.prevent>
-      <input type="text">
+      <!-- v-model データを双方向でバインディング -->
+      <input type="text" v-model="newItem">
+      <!-- @click DOMイベント指定 -->
       <button @click="addItem">Add</button>
     </form>
+    <pre>{{$data}}</pre>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    // returnする必要がある
+    return{
+      newItem: "",
+    }
+  },
+  // メソッド
   methods: {
     addItem(){
       alert();
