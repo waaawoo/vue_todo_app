5c258193697c349deaf60ee07c53651ce09423cd
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 12:41:02 +0900

    form入力　v-forでの出力

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,6 +7,11 @@
       <!-- @click DOMイベント指定 -->
       <button @click="addItem">Add</button>
     </form>
+    <!-- リスト表示 -->
+    <ul>
+      <!-- v-forはkeyを指定する必要がある -->
+      <li v-for="(todo, index) in todos" :key="index">{{index + 1}}:{{todo.item}}</li>
+    </ul>
     <pre>{{$data}}</pre>
   </div>
 </template>
@@ -17,12 +22,27 @@ export default {
     // returnする必要がある
     return{
       newItem: "",
+      // TODO用の配列
+      todos: [],
     }
   },
   // メソッド
   methods: {
     addItem(){
-      alert();
+      // 入力がからなら処理終了
+      if (this.newItem == ""){
+        alert("未入力です");
+        return;
+      }
+
+      // alert();
+      let todo = {
+        item: this.newItem
+      };
+      // TODOを配列へ格納
+      this.todos.push(todo);
+      // 入力後入力をからにする
+      this.newItem = "";
     }
   }
 }
