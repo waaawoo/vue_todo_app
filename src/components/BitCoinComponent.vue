97b17fed7babc142fe9e11dd2622b97d5bbcec89
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 15:11:42 +0900

    ビットコインの表示用ファイル作成

---
+++ src/components/BitCoinComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>ビットコイン価格表示</h2>
+  </div>
+</template>
+
+<script>
+export default({
+  data(){
+    return{
+      bpi: null,
+    }
+  },
+  mounted: function(){
+
+  }
+});
+
+</script>
+
+<style scope>
+
+</style>
