16fe6785e68d3788cd0e7b540dd89442248cb516
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 18:56:13 +0900

    監視プロパティで距離の算出

--- src/components/WatchComponent.vue
+++ src/components/WatchComponent.vue
@@ -7,6 +7,11 @@
     </p>
     <p>新しい入力 {{ newMessage }} </p>
     <p>古い入力 {{ old }} </p>
+
+    <h2>距離の出力</h2>
+    <p><input type="text" v-model="km">km</p>
+    <p><input type="text" v-model="m">m</p>
+    <p><input type="text" v-model="mm">mm</p>
   </div>
 </template>
 
@@ -18,6 +23,9 @@ export default {
       message: "vuejs",
       old: "",
       newMessage: "",
+      km: 0,
+      m: 0,
+      mm: 0,
     };
   },
   watch: {
@@ -25,7 +33,23 @@ export default {
       console.log(`new: ${newValue}, old: ${oldValue}`)
       this.old = oldValue
       this.newMessage = newValue
-    }
+    },
+    km: function(value){
+      this.km = value
+      this.m = value * 1000
+      this. mm = value * 100000
+    },
+    m: function(value){
+      this.km = value / 1000
+      this.m = value
+      this. mm = value * 1000
+    },
+    mm: function(value){
+      this.km = value / 1000000
+      this.m = value / 1000
+      this. mm = value
+    },
+
   },
   mounted() {
 
