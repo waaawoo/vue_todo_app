cfb51967e3f05b4545bd52eebe35571e1fe48dc1
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 23:29:13 +0900

    QiitaAPI検索機能完了

--- src/components/qiitaApiComponent.vue
+++ src/components/qiitaApiComponent.vue
@@ -1,25 +1,29 @@
 <template>
-  <div>
+  <div class="qiitaAPI">
+    <h2>Qiitaの記事を検索できます</h2>
     <p>
-      <input type="text" v-model="keyword">
+      <input type="text" v-model="keyword" />
     </p>
     <p>
-      {{message}}
+      {{ message }}
     </p>
-    <ul>
+    <ul class="qiitaAPI__UL">
       <li v-for="(item, index) in items" :key="index">
-        <a v-bind:href="item.url" target="_blank"> {{ item.title }} </a>
+        <a v-bind:href="item.url" target="_blank"
+          >{{ index + 1 }}『 {{ item.title }}』
+        </a>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+// Lodashのいんぽーと
+import _ from "lodash";
 // axiosの呼び出し
-import axios from "axios"
+import axios from "axios";
 
 export default {
-
   data() {
     return {
       items: null,
@@ -27,41 +31,57 @@ export default {
       message: "",
     };
   },
-  mounted() {
-
-  },
-  created(){
-    this.keyword = "javascript"
-    this.getAnswer()
+  mounted() {},
+  created() {
+    // this.keyword = "javascript"
+    // this.getAnswer()
+    // 入力中は処理が走らない
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 2000);
   },
   methods: {
-    getAnswer(){
-      if(this.keyword  === ""){
-        this.items = null
-        return
+    getAnswer() {
+      if (this.keyword === "") {
+        this.items = null;
+        this.message = "";
+        return;
       }
       //
-      this.message = "loding..."
-      let vm = this
-      let params = {page: 1, per_page: 20, query: this.keyword }
-      axios.get('https://qiita.com/api/v2/items', { params })
-      .then((response) => {
-        console.log(response.data)
-        vm.items = response.data
-      })
-      .catch((error) => {
-        vm.message = `ここにはいいてるError  : ${error}`
-      })
-      .finally(() => {
-        vm.message = ""
-      })
-
+      this.message = "loding...";
+      let vm = this;
+      let params = { page: 1, per_page: 20, query: vm.keyword };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then((response) => {
+          console.log(response);
+          vm.items = response.data;
+        })
+        .catch((error) => {
+          vm.message = `ここにはいいてるError  : ${error}`;
+        })
+        .finally(() => {
+          vm.message = "";
+        });
+    },
+  },
+  watch: {
+    keyword() {
+      this.message = "メッセージ入力中...";
+      this.debouncedGetAnswer();
     },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+  .qiitaAPI .qiita{
+    width: 80%;
+    margin: 0 auto;
+  }
 
-
+  .qiitaAPI__UL{
+    text-align: left;
+  }
+  .qiitaAPI__UL li{
+    margin-top: 5px;
+  }
 </style>
