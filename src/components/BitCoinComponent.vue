5ccd66b0b9f4356246bb3d409a7d7c9633b020f5
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-08 15:28:57 +0900

    axiosをインポートしてデータを取り出し、出力

--- src/components/BitCoinComponent.vue
+++ src/components/BitCoinComponent.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <h2>ビットコイン価格表示</h2>
+    <p> {{bpi}} </p>
   </div>
 </template>
 
 <script>
+// axiosの呼び出し
+import axios from "axios"
+
 export default({
   data(){
     return{
@@ -12,7 +16,12 @@ export default({
     }
   },
   mounted: function(){
-
+    axios.get(" https://api.coindesk.com/v1/bpi/currentprice/CNY.json Sample JSON")
+    .then(function(response){
+      console.log(response.data.bpi.USD.rate_float);
+      this.bpi = response.data.bpi.USD.rate_float
+      // vueインスタンスのthisを指定するためにbindする必要がある
+    }.bind(this))
   }
 });
 
